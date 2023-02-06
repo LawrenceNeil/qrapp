14193faae9bed389dd6dd9780236701d2b560be5
Lawrence Neil Limboc <limboc012@gmail.com>
2023-02-06 12:50:32 +0800

    minor changes

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route
-          path="/dashboard"
+          path="qrapp/dashboard"
           element={
             <RequireAuth loginPath={"/"}>
               <Dashboard />
